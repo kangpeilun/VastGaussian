6c0aabda6ab92e499b2a6c5b67761d20bc2b6f63
kpl <374774222@qq.com>
2024-06-18 16:40:29 +0800

    fix multi gpu

--- train_multigpu.py
+++ train_multigpu.py
@@ -71,7 +71,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
 
     viewpoint_stack = None
     ema_loss_for_log = 0.0
-    progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
+    progress_bar = tqdm(range(first_iter, opt.iterations), desc=f"Training progress Partition {dataset.partition_id}")
     first_iter += 1
     for iteration in range(first_iter, opt.iterations + 1):        
         if network_gui.conn == None:
