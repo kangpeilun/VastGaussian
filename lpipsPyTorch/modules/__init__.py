be6578764cd97a829c3323e947c5a00011e76488
Kang Peilun <374774222@qq.com>
2024-06-21 16:14:35 +0800

    Refactor

---
+++ lpipsPyTorch/modules/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#        Data: 2024-06-21 15:39
+#     Project: VastGaussian
+#   File Name: __init__.py
+#      Author: KangPeilun
+#       Email: 374774222@qq.com 
+# Description: 
